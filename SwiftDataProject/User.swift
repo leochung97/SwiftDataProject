b23472bdd7f4a22d4029a9ce2204e68d45995008
Leo Chung <leo.chung97@gmail.com>
2024-01-11 16:19:59 -0500

    Add starter user form

---
+++ SwiftDataProject/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Leo Chung on 1/11/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
