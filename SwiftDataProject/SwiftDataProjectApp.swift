bfa955a518b2b6a800d9af12bf22305f34b70ed9
Leo Chung <leo.chung97@gmail.com>
2024-01-11 12:45:57 -0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Leo Chung on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
