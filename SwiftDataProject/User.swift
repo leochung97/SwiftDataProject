3197b81218c8ed8b3068a707da15a841146a551b
Leo Chung <leo.chung97@gmail.com>
2024-01-12 14:38:53 -0500

    Project wrap up

--- SwiftDataProject/User.swift
+++ SwiftDataProject/User.swift
@@ -10,13 +10,42 @@ import SwiftData
 
 @Model
 class User {
-    var name: String
-    var city: String
-    var joinDate: Date
+    var name: String = "Anonymous"
+    var city: String = "Unknown"
+    var joinDate: Date = Date.now
+    @Relationship(deleteRule: .cascade) var jobs: [Job]? = [Job]()
+    // var jobs = [Job]()
     
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
         self.joinDate = joinDate
+    }
+}
+
+// Note: adding a new relational model to User -> SwiftData will silently add jobs property to all of its existing users, giving them an empty array by default
+// This is called  amigration: when we add or delete properties in our models
+// You can also do custom migrations to handle bigger model changes
+
+/*
+ Note: if you delete a user, the jobs data will remain intact unless you use a @Relationship macro, providing it with a delete rule that describes how Job objects should be handled when their owning User is deleted
+ The default delete rule is called .nullify, which means the owner property of each Job object gets set to nil, marking that they have no owner
+ You can use .cascade, which means deleting a User should automatically delete all of their Job objects -> it's called cascade because the delete keeps going for all related objects
+*/
+
+/*
+ For iCloud capability -> purchase a paid developer program account on Apple
+ Note: SwiftData with iCloud has a requirement -> all properties must be optional or have default values, and all relationships must be optional
+*/
+@Model
+class Job {
+    var name: String = "None"
+    var priority: Int = 1
+    var owner: User?
+
+    init(name: String, priority: Int, owner: User? = nil) {
+        self.name = name
+        self.priority = priority
+        self.owner = owner
     }
 }
--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -23,10 +23,34 @@ struct UsersView: View {
 //    }
 //    
     @Query var users: [User]
+    @Environment(\.modelContext) var modelContext
+    
+    func addSample() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+
+        modelContext.insert(user1)
+
+        user1.jobs?.append(job1)
+        user1.jobs?.append(job2)
+    }
     
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+
+                Spacer()
+
+                Text(String(user.jobs?.count ?? 0))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
     }
 }
